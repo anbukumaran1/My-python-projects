445dfb8b984900e8f69e7d4ba871041384c40ac9
Anbukumaran Arangaperumal Govindasamy Rangasamy <148788136+anbukumaran1@users.noreply.github.com>
2023-10-24 13:26:11 +0530

    Add files via upload

---
+++ muliplication table.py
@@ -0,0 +1,6 @@
+import time
+num = int(input("Display multiplication table of : "))
+ti=int(input("Enter number of times to diasplay :"))
+for i in range(1,ti ):
+   print(num, 'x', i, '=', num*i)
+time.sleep(10000000000)
